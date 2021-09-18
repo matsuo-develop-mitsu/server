2f32fed0b23f89de5d727def4ded7366e49cc307
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2021-09-18 13:03:40 +0900

    changed index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send({ hi: "hello" });
+  res.send({ bye: "bye" });
 });
 
 const PORT = process.env.PORT || 5001;
