204f40e723f12626921e18e67fc65d741abf8125
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2021-09-23 18:09:46 +0900

    finish auth method

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/key.js");
+require("./models/User");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes.js");
+
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send({ bye: "bye" });
-});
+app.use(
+  cookieSession({
+    maxAge: 60 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 引数でappを入れることにより、ローカル変数としてauthRoutesで実行され、routesファイルの実行を行える
+authRoutes(app);
+
+// mongoDBに接続する
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
+
+// mongo userName mongodb+srv://dbAka:<password>@cluster0.sqkhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
